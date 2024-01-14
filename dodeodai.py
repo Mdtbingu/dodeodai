f7aa8ed3f7aeb2c436ce8b298e0d7d99fcf952e2
Đăng Minh <122171358+Mdtbingu@users.noreply.github.com>
2024-01-14 01:33:22 -0700

    úp đết

--- dodeodai.py
+++ dodeodai.py
@@ -1,6 +1,7 @@
 import sys
-sys.set_int_max_str_digits(0)
+sys.set_int_max_str_digits(0) #đặt lại giới hạn string
 
+# Chia để trị xử lý lũy thừa lớn
 def luythua(n,s):
     if s==1:
         return n
@@ -10,8 +11,10 @@ def luythua(n,s):
 
 
 def solution(a,u,k):
+    #Lập mảng quy hoạch động
     for i in range(1,len(a)):
         a[i]*=a[i-1]
+    #Dùng vòng lặp để chạy qua tất cả Truy vấn
     res=[]
     for i in range(len(u)):
         if u[i]==1 and k[i]!=1: res.append((a[u[i]+k[i]-2])%(10**9+7))
@@ -20,7 +23,7 @@ def solution(a,u,k):
         else: res.append((a[u[i]+k[i]-2]*luythua(a[u[i]-2],10**9+5))%(10**9+7))
     return res
 
-
+#đọc file và ghi ra file
 
 with open('dodeodai.inp', mode='r') as file_in:
     file_input=file_in.read().split('\n')
